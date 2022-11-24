07ed548a1469e3ca6fe8dc2ab31d6309d76ce4eb
Elvis Espinoza <elvis.espinoza.camargo@gmail.com>
2022-11-24 15:19:04 -0500

    modify mockData

--- src/Service/MockData.js
+++ src/Service/MockData.js
@@ -18,43 +18,14 @@ export const TaskData = [
     isComplete: true,
     createdDate: "10-10-2022",
     completedDate: "10-10-2022"
-  },
-  {
-    company: "Square Inc",
-    ticker: "SQ",
-    stockPrice: "45.28 USD",
-    timeElapsed: "10 sec ago",
-  },
-  {
-    company: "Shopify Inc",
-    ticker: "SHOP",
-    stockPrice: "341.79 USD",
-    timeElapsed: "3 sec ago",
-  },
-  {
-    company: "Sunrun Inc",
-    ticker: "RUN",
-    stockPrice: "9.87 USD",
-    timeElapsed: "4 sec ago",
-  },
-  {
-    company: "Adobe Inc",
-    ticker: "ADBE",
-    stockPrice: "300.99 USD",
-    timeElapsed: "10 sec ago",
-  },
-  {
-    company: "HubSpot Inc",
-    ticker: "HUBS",
-    stockPrice: "115.22 USD",
-    timeElapsed: "12 sec ago",
-  },
+  }
 ];
 
 export const AccountData = {
   personalData: {
     userName: "vam1832",
-    uriProfilePhoto: "",
-    birthdate: "",
+    uriProfilePhoto: "https://media-exp1.licdn.com/dms/image/C4E03AQE3lb68KUqTnA/profile-displayphoto-shrink_100_100/0/1650290273743?e=1674691200&v=beta&t=XtnVm9AZoH-opRqa5skjTN57wdyANec_5T-d-6ucP3w",
+    birthdate: "1990-10-10",
+
   }
 }
